f6db14e1f333fe5f79b6561955e5c5622dc9f85e
olensis <olensis7gmail@.com>
2022-11-11 17:26:16 +0300

    запуталась

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Сheck p1 = new Сheck("111", "222", "иии");
+        System.out.println(p1);
+
+
+
     }
 }
